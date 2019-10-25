5cb3b0bec2aa5a6b76c18b87d354ecbab0ba020c
Adam Szi <szi.adam@simonyi.bme.hu>
2019-10-25 20:52:28 +0200

    basic gameplay done

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    private Rigidbody2D _rigidbody;
+
+    public float speed = 9f;
+    public Vector3 startVelocity = new Vector3(1, 0, 0);
+    [SerializeField] private Vector3 vel;
+
+    private void Awake() {
+        this._rigidbody = this.GetComponent<Rigidbody2D>();
+        this._rigidbody.velocity = this.startVelocity;
+    }
+
+    private void Update() {
+        _rigidbody.velocity = _rigidbody.velocity.normalized * speed;
+        this.vel = _rigidbody.velocity;
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+
+    public float speed = 1f;
+    private float x;
+
+    private void Awake() {
+        this._transform = transform;
+        this._rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        this.x = Input.GetAxis("Horizontal");
+        Move();
+    }
+
+    private void Move() {
+        this._rigidbody.velocity = new Vector3(this.x * this.speed, 0, 0);
+    }
+}
